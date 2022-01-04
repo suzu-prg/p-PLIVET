912dac6b82b4b66eb3a754bafdc66e7cb9065cf7
suzu-prg <44055284+suzu-prg@users.noreply.github.com>
2022-01-04 20:09:57 +0000

    Deploying to gh-pages from @ suzu-prg/p-PLIVET@9be44879cbf65c546e3fd77a186ad4b0335f1a7b 🚀

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-// import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AppWithLang from './components/AppWithLang';
-
-ReactDOM.render(<AppWithLang />, document.getElementById('root'));
--- test/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import AppWithLang from '../src/components/AppWithLang';
-
-Enzyme.configure({ adapter: new Adapter() });
-it('shallow renders without crashing', () => {
-  shallow(<AppWithLang />);
-});
